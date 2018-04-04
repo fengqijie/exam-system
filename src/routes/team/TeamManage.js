97281704c121452eeed920bddea6f1170230ca0f
wencaizhang <1052642137@qq.com>
2018-04-05 00:23:08 +0800

    完善组织机构

--- src/routes/team/TeamManage.js
+++ src/routes/team/TeamManage.js
@@ -2,208 +2,92 @@ import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'dva';
 import moment from 'moment';
 import {
-  Row,
   Col,
   Card,
   Form,
-  Input,
-  Select,
   Icon,
-  Upload,
+  Input,
   Button,
-  TreeSelect,
   Dropdown,
-  InputNumber,
-  DatePicker,
   Modal,
   message,
-  Badge,
-  Divider,
+  Table,
 } from 'antd';
-import StandardTable from 'components/StandardTable';
-import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
-import AddUser from './UserAdd';
-
-const WrappedRegistrationForm = Form.create()(AddUser);
-
-import styles from './TeamManage.less';
-
+const ButtonGroup = Button.Group;
+const confirm = Modal.confirm;
 const FormItem = Form.Item;
-const { Option } = Select;
-const getValue = obj =>
-  Object.keys(obj)
-    .map(key => obj[key])
-    .join(',');
-const statusMap = ['default', 'processing', 'success', 'error'];
-const status = ['关闭', '运行中', '已上线', '异常'];
-const columns = [
-  {
-    title: '序号',
-    dataIndex: 'key',
-  },
-  {
-    title: '姓名',
-    dataIndex: 'owner',
-  },
-  {
-    title: '登陆账号',
-    dataIndex: 'no',
-  },
-  {
-    title: '身份证号',
-    dataIndex: 'status'
-  },
-  {
-    title: '工号',
-    dataIndex: 'updatedAt',
-    sorter: true,
-    render: val => <span>{moment(val).format('YYYY-MM-DD HH:mm:ss')}</span>,
-  },
-  {
-    title: '岗位',
-    dataIndex: 'title',
-  },
-  {
-    title: '岗位兼职',
-    dataIndex: 'description',
-  },
-  {
-    title: '联系电话',
-    dataIndex: 'href',
-  },
-  {
-    title: '所属组织',
-    dataIndex: 'callNo',
-  },
-  {
-    title: '照片',
-    render: () => (
-      <Fragment>
-        <a href="{{avatar}}">avatar</a>
-      </Fragment>
-    ),
-  },
-  {
-    title: '操作',
-    render: () => (
-      <Fragment>
-        <a href="">修改</a>
-        <Divider type="vertical" />
-        <a href="">删除</a>
-      </Fragment>
-    ),
-  },
-];
 
-const uploadProps = {
-  name: 'file',
-  action: '//jsonplaceholder.typicode.com/posts/',
-  headers: {
-    authorization: 'authorization-text',
-  },
-  onChange(info) {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
+import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import styles from './TeamManage.less';
+import MainTreeSelect from './MainTreeSelect';
+import DeleteModal from './DeleteModal';
 
-const CreateForm = Form.create()(props => {
-  const { modalVisible, form, handleAdd, handleModalVisible } = props;
-  const okHandle = () => {
-    form.validateFields((err, fieldsValue) => {
-      if (err) return;
-      form.resetFields();
-      handleAdd(fieldsValue);
-    });
-  };
-  return (
-    <Modal
-      title="新增用户基本信息"
-      visible={modalVisible}
-      onOk={okHandle}
-      onCancel={() => handleModalVisible()}
-    >
-      <WrappedRegistrationForm />
-    </Modal>
-  );
-});
+const FormModal = Form.create()(
+  class extends React.Component {
+    render() {
+      const { data, visible, onCancel, onCreate, form } = this.props;
+      const { getFieldDecorator } = form;
+      const targetData = data.filter(item => item.isCurrent)[0] || {};
+      const props = targetData.isEditing
+      ? { title: `修改职位：${targetData.job}`, okText: '修改' }
+      : { title: `为职位：${targetData.job} 增加下一级`, okText: '增加' }
+
+      return (
+        <Modal
+          visible={visible}
+          title="提示"
+          okText="确定"
+          onCancel={onCancel}
+          onOk={onCreate}
+          {...props}
+        >
+          <Form layout="vertical">
+            <FormItem label={<span>职位</span>}>
+              {getFieldDecorator('job', {
+                rules: [{ required: true, message: '请输入职位', whitespace: true }],
+              })(<Input placeholder='请输入职位' />)}
+            </FormItem>
+            <FormItem label={<span>备注</span>}>
+              {getFieldDecorator('remark', {
+                rules: [{ required: false, message: '请输入备注', whitespace: true }],
+              })(<Input.TextArea placeholder='请输入备注' />)}
+            </FormItem>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+);
 
 @connect(({ rule, loading }) => ({
   rule,
   loading: loading.models.rule,
 }))
+
 @Form.create()
+
 export default class TableList extends PureComponent {
   state = {
-    modalVisible: false,
-    expandForm: false,
-    isLoadTreeData: true,
-    selectedRows: [],
-    formValues: {},
-    value: [],
-    treeData: [
-      {
-        label: 'Node1',
-        value: '0-0',
-        key: '0-0',
-        children: [
-          {
-            label: 'Child Node1',
-            value: '0-0-0',
-            key: '0-0-0',
-          },
-          {
-            label: 'Child Node6',
-            value: '0-0-1',
-            key: '0-0-1',
-          },
-          {
-            label: 'Child Node7',
-            value: '0-0-2',
-            key: '0-0-2',
-          },
-          {
-            label: 'Child Node8',
-            value: '0-0-3',
-            key: '0-0-3',
-          },
-          {
-            label: 'Child Node9',
-            value: '0-0-4',
-            key: '0-0-4',
-          },
-        ],
-      },
-      {
-        label: 'Node2',
-        value: '0-1',
-        key: '0-1',
-        children: [
-          {
-            label: 'Child Node3',
-            value: '0-1-0',
-            key: '0-1-0',
-          },
-          {
-            label: 'Child Node4',
-            value: '0-1-1',
-            key: '0-1-1',
-          },
-          {
-            label: 'Child Node5',
-            value: '0-1-2',
-            key: '0-1-2',
-          },
-        ],
-      },
+    data: [
+      {key: 1, name: '领导1', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环', children: [
+        {key: 12, name: '小领导2', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+        {key: 13, name: '小领导3', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+        {key: 14, name: '小领导4', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+        {key: 15, name: '小领导5', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+      ]},
+      {key: 2, name: '领导2', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+      {key: 3, name: '领导3', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+      {key: 4, name: '领导4', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+      {key: 5, name: '领导5', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'},
+      {key: 6, name: '领导6', manager: '张三', id: 123, phone: 123, address: '石家庄市长安区北二环'}
     ],
+    treeData: [],
+    value: [],
+    visibleFormModal: false,
+    visibleDeleteModal: false,
+    deleteModalText: '',
+    confirmLoading: false,
   };
 
   componentDidMount() {
@@ -211,161 +95,164 @@ export default class TableList extends PureComponent {
     dispatch({
       type: 'rule/fetch',
     });
+  } 
+
+  columns = [
+    {
+      title: '组织名称',
+      dataIndex: 'name',
+    }, {
+      title: '组织代码',
+      dataIndex: 'id',
+    }, {
+      title: '地址',
+      dataIndex: 'address',
+    }, {
+      title: '联系方式',
+      dataIndex: 'phone',
+    }, {
+      title: '操作',
+      dataIndex: 'operation',
+      render: (text, record) => {
+        const { editable } = record;
+        return (
+          <ButtonGroup>
+            <Button icon="plus" onClick={() => this.handleAdd(record.key)}>增加下级组织</Button>
+            <Button icon="edit" onClick={() => this.handleEdit(record.key)}>修改</Button>
+            <Button icon="delete" onClick={() => this.handleDelete(record.key)} type="danger">删除</Button>
+          </ButtonGroup>
+        );
+      },
+    }
+  ];
+
+  handleAdd = (key) => {
+    const newData = [...this.state.data];
+    newData.forEach(item => {
+      item.isCurrent = item.key === key;
+      item.isEditing = false;
+    })
+    this.setState({ data: newData, visibleFormModal: true });
   }
 
-  handleStandardTableChange = (pagination, filtersArg, sorter) => {
-    const { dispatch } = this.props;
-    const { formValues } = this.state;
+  handleDelete = (key) => {
+    const newData = [...this.state.data];
+    newData.forEach(item => item.isCurrent = item.key === key )
+    const filterData = newData.filter(item => item.isCurrent)[0];
+    let text = `即将删除职位：${filterData.job} 及其下属职位`;
+    this.setState({ visibleDeleteModal: true, deleteModalText: text });
+  }
 
-    const filters = Object.keys(filtersArg).reduce((obj, key) => {
-      const newObj = { ...obj };
-      newObj[key] = getValue(filtersArg[key]);
-      return newObj;
-    }, {});
+  handleEdit(key) {
+    const newData = [...this.state.data];
+    newData.forEach(item => {
+      item.isCurrent = item.key === key;
+      item.isEditing = true;
+    })
+    this.setState({ data: newData, visibleFormModal: true });
+  }
 
-    const params = {
-      currentPage: pagination.current,
-      pageSize: pagination.pageSize,
-      ...formValues,
-      ...filters,
-    };
-    if (sorter.field) {
-      params.sorter = `${sorter.field}_${sorter.order}`;
-    }
-
-    dispatch({
-      type: 'rule/fetch',
-      payload: params,
-    });
-  };
-
-  handleFormReset = () => {
-    const { form, dispatch } = this.props;
-    form.resetFields();
-    this.setState({
-      formValues: {},
-    });
-    dispatch({
-      type: 'rule/fetch',
-      payload: {},
-    });
-  };
-
-  handleSelectRows = rows => {
-    this.setState({
-      selectedRows: rows,
-    });
-  };
-  
-  onTreeSelectChange = value => {
-    this.setState({ value });
-  };
-
-  handleSearch = e => {
+  // about tree-select
+  handleTreeSelectSearch = e => {
     e.preventDefault();
-    console.log(this.state.value)
+    console.log(this.state.value);
   };
 
-  handleModalVisible = flag => {
-    this.setState({
-      modalVisible: !!flag,
-    });
-  };
+  // about tree-select
+  handleTreeSelectChange = value => {
+    this.setState({ value });
+  }
 
-  handleDownloadTemplate = () => {
-    message.info('下载 execl 模板');
-  };
-
-  handleAdd = fields => {
-    this.props.dispatch({
-      type: 'rule/add',
-      payload: {
-        description: fields.desc,
-      },
-    });
-
-    message.success('添加成功');
-    this.setState({
-      modalVisible: false,
-    });
-  };
-
-  renderForm() {
-    let searchSize = 'large';
+  // about tree-select
+  renderTreeSelect () {
     const tProps = {
       treeData: this.state.treeData,
-      allowClear: true,
-      treeCheckStrictly: true,
-      size: searchSize,
       value: this.state.value,
-      onChange: this.onTreeSelectChange,
-      treeCheckable: true,
-      showCheckedStrategy: TreeSelect.SHOW_ALL,
-      searchPlaceholder: 'Please select',
+      onChange: this.handleTreeSelectChange,
+      onSearch: this.handleTreeSelectSearch
     };
-
     return (
-      <Form onSubmit={this.handleSearch} layout="inline">
-        <Row className={styles.mainSearch} gutter={{ md: 8, lg: 24, xl: 48 }}>
-          <Col className={styles.treeSelect} md={10} sm={20}>
-            <TreeSelect {...tProps} />
-          </Col>
-          <Col className={styles.searchButton} md={2} sm={4}>
-            <span className={styles.submitButtons}>
-              <Button loading={!this.state.isLoadTreeData} size={searchSize} type="primary" htmlType="submit">
-                查询
-              </Button>
-            </span>
-          </Col>
-        </Row>
-      </Form>
+      <div style={{ textAlign: 'center' }}>
+        <MainTreeSelect {...tProps}/>
+      </div>
     );
   }
 
-  renderAddUserButtons () {
-    return (
-      <Fragment>
-        <Button icon="plus" type="primary"
-          onClick={() => this.handleModalVisible(true)}
-        >
-          新增
-        </Button>
-        <Upload {...uploadProps}>
-          <Button icon="upload" type="primary">批量导入用户信息</Button>
-        </Upload>
-        <Button icon="download" type="primary"
-          onClick={() => this.handleDownloadTemplate()}
-        >
-          execl 模板下载
-        </Button>
-      </Fragment>
-    );
+  // modal
+  handleFormModalCancel = () => {
+    const form = this.formRef.props.form
+    form.resetFields();
+    this.setState({ visibleFormModal: false });
+  }
+  handleFormModalCreate = () => {
+    const form = this.formRef.props.form
+    form.validateFields((err, values) => {
+      if (err) { return; }
+
+      form.resetFields();
+      this.setState({ visibleFormModal: false });
+      console.log('Received values of form: ', values);
+
+      this.handleData(values);
+    });
+  }
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
+  }
+
+  handleData (values) {
+    const newData = [...this.state.data];
+    newData.forEach(item => {
+      if (item.isCurrent) {
+        if (item.isEditing) {
+          Object.assign(item, values);
+          message.success('修改成功！')
+        } else {
+          item.children = item.children || [];
+          item.children.push(values);
+          message.success('添加成功！')
+        }
+      }
+    });
+    this.setState({ data: newData });
+  }
+
+  handleDeleteModalCancel = () => {
+    this.setState({ visibleDeleteModal: false });
+  }
+  handleDeleteModalOk = () => {
+    const newData = [...this.state.data];
+    const filterData = newData.filter(item => item.isCurrent)[0];
+    console.log(filterData)
+    
+    this.setState({ confirmLoading: true });
+
+    setTimeout(() => {
+      message.success('删除成功');
+      this.setState({ visibleDeleteModal: false, confirmLoading: false });
+    }, 2000);
   }
 
   render() {
-    const { rule: { data }, loading } = this.props;
-    const { selectedRows, modalVisible } = this.state;
-
-    const parentMethods = {
-      handleAdd: this.handleAdd,
-      handleModalVisible: this.handleModalVisible,
-    };
-
     return (
-      <PageHeaderLayout title="组织结构管理" content={this.renderForm()}>
-        <Card bordered={false}> 
-          <div className={styles.tableList}>
-            <StandardTable
-              selectedRows={selectedRows}
-              loading={loading}
-              data={data}
-              columns={columns}
-              onSelectRow={this.handleSelectRows}
-              onChange={this.handleStandardTableChange}
-            />
-          </div>
+      <PageHeaderLayout title="组织结构管理" content={this.renderTreeSelect()}>
+        <Card bordered={false}>
+          <Table bordered dataSource={this.state.data} columns={this.columns} />
+          <FormModal
+            wrappedComponentRef={this.saveFormRef}
+            visible={this.state.visibleFormModal}
+            onCancel={this.handleFormModalCancel}
+            onCreate={this.handleFormModalCreate}
+            data={this.state.data}
+          />
+          <DeleteModal 
+            visible={this.state.visibleDeleteModal}
+            onOk={this.handleDeleteModalOk}
+            onCancel={this.handleDeleteModalCancel}
+            confirmLoading={this.state.confirmLoading}
+            ModalText={this.state.deleteModalText}
+          />
         </Card>
-        <CreateForm {...parentMethods} modalVisible={modalVisible} />
       </PageHeaderLayout>
     );
   }
